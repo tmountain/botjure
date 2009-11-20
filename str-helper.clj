1fe7443c809691605f6af3f1662a706ff429237d
Jeremy James <jjames@li112-65.members.linode.com>
2009-11-20 21:26:47 +0000

    omg forgot the helper file

---
+++ str-helper.clj
@@ -0,0 +1,19 @@
+(ns str-helper)
+(defn parse-msg [line]
+  (let [[user type channel msg] (seq (.split line " " 4))]
+    (let [[username n] (seq (.split user "!"))]
+      {:from (if username (.substring username 1) nil),
+       :n n,
+       :to channel,
+       :with type,
+       :msg (if msg (.substring msg 1) nil)})))
+
+(defmacro str-include? 
+  "Returns true if string contains match or false otherwise"
+  [string match]
+  `(-> (. ~string toLowerCase) (. contains (. ~match toLowerCase))))
+
+(defmacro str-startswith? 
+  "Returns true if string starts with match or false otherwise"
+  [string match]
+  `(-> (. ~string toLowerCase) (. startsWith (. ~match toLowerCase))))
