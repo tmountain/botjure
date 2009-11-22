c670d2da925902d24469366f0f935ff9ba9d5977
Travis Whitton <travis@travis-desktop.(none)>
2009-11-22 11:49:03 -0500

    much cleaner dispatch function

--- plugin_loader.clj
+++ plugin_loader.clj
@@ -1,8 +1,8 @@
 (ns plugin-loader
   (:use str-helper))
 
-(def plugins '(plugins counter echo))
-(require plugins)
+(def plugins '(plugins.counter plugins.echo))
+(apply require plugins)
 
 (defn dispatch [plugins msg]
   (loop [plugins plugins
@@ -11,13 +11,10 @@
     (let [plugin (first plugins)]
       (if (not plugin)
         payload
-        (if (not (= 'plugins plugin))
-          (let [properties 
-                (eval (symbol (str "plugins." plugin "/properties")))]
-            (if (or (= (:matches properties) :all)
-                    (not (empty? (filter #(= msg %1) (:matches properties)))))
-              (recur (rest plugins) msg 
-                     (conj payload (merge {:plugin (:name properties)}
-                                          ((:dispatch properties) (parse-msg msg)))))
-              (recur (rest plugins) msg payload)))
-          (recur (rest plugins) msg payload))))))
+        (let [properties (var-get (ns-resolve plugin 'properties))]
+          (if (or (= (:matches properties) :all)
+                  (not (empty? (filter #(= msg %1) (:matches properties)))))
+            (recur (rest plugins) msg 
+                   (conj payload (merge {:plugin (:name properties)}
+                                        ((:dispatch properties) (parse-msg msg)))))
+            (recur (rest plugins) msg payload)))))))
