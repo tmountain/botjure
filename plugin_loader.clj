39cbbe450072e7b0ca95737713199394ebf044ce
Jeremy James <jjames@li112-65.members.linode.com>
2009-11-23 21:59:07 +0000

    broken effort to add parsing to msgs

--- plugin_loader.clj
+++ plugin_loader.clj
@@ -19,14 +19,14 @@
       (if (not plugin)
         payload
         (let [properties (var-get (ns-resolve plugin 'properties))
-              msg-txt (if (contains? msg :msg) (:msg msg) nil)
+              msg-txt (if (contains? msg :msg) (:txt (:msg msg)) nil)
               msg-from (if (contains? msg :from) (:from msg) nil)]
           ; in order for a plugin to dispatch, the conditions must be met:
           ; msg-txt & msg-from are non-nil values
           ; the message isn't from the bot
           ; the plugin's :matches value is within the messages text
           (if (and msg-txt msg-from (not (= msg-from (:bot-name config)))
-            (apply-match (:msg msg) (:matches properties) (:bot-name config)))
+            (apply-match msg-txt (:matches properties) (:bot-name config)))
             (recur (rest plugins) msg 
                    (conj payload (merge {:plugin (:name properties)}
                                         ((:dispatch properties) msg))))
--- str_helper.clj
+++ str_helper.clj
@@ -1,27 +1,52 @@
 (ns str-helper)
 
-(defn parse-msg 
-  [line]
+;; (defn parse-msg 
+;;   [line]
+;;   "Returns a map of extracted values from a message"
+;;   (let [[user type channel msg] (seq (.split line " " 4))]
+;;     (let [[username n] (seq (.split user "!"))]
+;;       (let [from (if username (.substring username 1) nil)
+;;             msg-txt (if msg (.substring msg 1) nil)]
+;;         (if msg-txt
+;;           (let [match (re-matcher #"(\S+):\s*(.*)" msg-txt)]
+;;             (if (re-find match)
+;;               (let [[_ username msg-trim] (re-groups match)]
+;;                 {:from from,
+;;                  :n n,
+;;                  :to channel,
+;;                  :with type,
+;;                  :addr username,
+;;                  :msg msg-trim})
+;;               {:from from,
+;;                :n n,
+;;                :to channel,
+;;                :with type,
+;;                :msg msg})))))))
+
+(defn parse-username-txt [username]
+     (if username
+       (.substring username 1)))
+
+(defn parse-msg-txt [msg]
+  (if msg
+    (let [msg-txt (.substring msg 1)
+          match (re-matcher #"(\S+):\s*(.*)" msg-txt)]
+      (if (re-find match)
+        (let [[_ addr txt] (re-groups match)]
+          {:txt txt
+           :addr addr})
+        {:txt msg-txt}))))
+
+(defn parse-msg [line]
   "Returns a map of extracted values from a message"
   (let [[user type channel msg] (seq (.split line " " 4))]
     (let [[username n] (seq (.split user "!"))]
-      (let [from (if username (.substring username 1) nil)
-            msg-txt (if msg (.substring msg 1) nil)]
-        (if msg-txt
-          (let [match (re-matcher #"(\S+):\s*(.*)" msg-txt)]
-            (if (re-find match)
-              (let [[_ username msg-trim] (re-groups match)]
-                {:from from,
-                 :n n,
-                 :to channel,
-                 :with type,
-                 :addr username,
-                 :msg msg-trim})
-              {:from from,
-               :n n,
-               :to channel,
-               :with type,
-               :msg msg-txt})))))))
+      {:from (parse-username-txt username)
+       :n n
+       :to channel
+       :with type
+       :msg (parse-msg-txt msg)})))
+
 
 (defmacro str-include? 
   "Returns true if string contains match or false otherwise"
