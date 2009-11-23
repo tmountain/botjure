1f0aa83b8959fb09baad7830a3e531ac0be6faef
Jeremy James <jjames@li112-65.members.linode.com>
2009-11-23 20:46:49 +0000

    unholy parse update to attempt to bring about a seperate address-ee and message OR the apocalypse

--- str_helper.clj
+++ str_helper.clj
@@ -5,11 +5,23 @@
   "Returns a map of extracted values from a message"
   (let [[user type channel msg] (seq (.split line " " 4))]
     (let [[username n] (seq (.split user "!"))]
-      {:from (if username (.substring username 1) nil),
-       :n n,
-       :to channel,
-       :with type,
-       :msg (if msg (.substring msg 1) nil)})))
+      (let [from (if username (.substring username 1) nil)
+            msg-txt (if msg (.substring msg 1) nil)]
+        (if msg-txt
+          (let [match (re-matcher #"(\S+):\s*(.*)" msg-txt)]
+            (if (re-find match)
+              (let [[_ username msg-trim] (re-groups match)]
+                {:from from,
+                 :n n,
+                 :to channel,
+                 :with type,
+                 :addr username,
+                 :msg msg-trim})
+              {:from from,
+               :n n,
+               :to channel,
+               :with type,
+               :msg msg-txt})))))))
 
 (defmacro str-include? 
   "Returns true if string contains match or false otherwise"
