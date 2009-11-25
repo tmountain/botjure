cdb589f3e8487ae1c58b4be5006881e706d1cc74
Jeremy James <jjames@li112-65.members.linode.com>
2009-11-25 18:18:51 +0000

    hello plugin now searches for call/response for more than just hello

--- plugins/hello.clj
+++ plugins/hello.clj
@@ -2,8 +2,17 @@
 (declare hello)
 
 (defn hello [arg]
-  {:payload (str "Hello " (:from arg) "!") :to (:to arg)})
+  {:payload (str "Hello, " (:from arg) "!") :to (:to arg)})
+
+(defn thank [arg]
+  {:payload (str "You are most welcome, " (:from arg) "!") :to (:to arg)})
+
+(defn hello-dispatch [arg]
+  (let [cmd (:cmd arg)]
+    (cond (re-find #"hello.*" cmd) (hello arg)
+          (re-find #"thank.*" cmd) (thank arg)
+          :else {})))
 
 (def properties {:name      "hello",
-                 :matches   ["hello"],
-                 :dispatch  hello })
+                 :matches   :all,
+                 :dispatch  hello-dispatch })
